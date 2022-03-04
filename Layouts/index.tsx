d8ea37f3e4db1d807b802c17ebc84a629354bfe1
Ngoc Thai <ngocthai@pixelcent.com>
2022-03-04 10:23:35 +0700

    update canonical

--- Layouts/index.tsx
+++ Layouts/index.tsx
@@ -12,8 +12,8 @@ const CommonLayout = ({ children }: Props) => {
   
   const router = useRouter();
   const _pathSliceLength = Math.min.apply(Math, [
-      router.asPath.indexOf('?') > 0 ? router.asPath.indexOf('?') : router.asPath.length,
-      router.asPath.indexOf('#') > 0 ? router.asPath.indexOf('#') : router.asPath.length
+    router.asPath.indexOf('?') > 0 ? router.asPath.indexOf('?') : router.asPath.length,
+    router.asPath.indexOf('#') > 0 ? router.asPath.indexOf('#') : router.asPath.length
   ]);
   const canonicalURL= process.env.DOMAIN + router.asPath.substring(0, _pathSliceLength);
 
