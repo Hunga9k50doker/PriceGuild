1b402dd180485276ffa8025862cd3066c6ca073c
Phong Ngô <tphong0204@gmail.com>
2022-03-18 11:53:16 +0700

    [FE]# update code

--- pages/unsubscribe/index.tsx
+++ pages/unsubscribe/index.tsx
@@ -7,38 +7,36 @@ import queryString from 'query-string';
 import { api } from 'configs/axios';
 import Skeleton from 'react-loading-skeleton';
 import { useRouter } from 'next/router';
-import { isEmpty } from 'lodash';
+import { head, isEmpty } from 'lodash';
+import Head from 'next/head';
 
-const Unsubscribe: React.FC = () => {
+const Unsubscribe: React.FC = ({...props}) => {
     const router = useRouter();
     const { code } = router.query;
     const [status, setStatus] = useState<boolean>(false);
     const [isLoading, setisLoading] = useState<boolean>(true);
+    const [title, setTitle] = useState<string>('');
+    const [message, setMessage] = useState<string>('');
     useEffect(() => {
         const restoreAccount = async () => {
             try {
             const params = {
                 token: code
             };
-                const result = await api.v1.account.unSubcribeAcounnt(params);
                 
-                setStatus(result.success);
-                setisLoading(false);
-                if (result.success) {
-                    let timer: any = null;
-
-                    if (timer) {
-                        clearTimeout(timer);
-                    }
-
-                    timer = setTimeout(() => {
-                       router.push('/profile/personal')
-                    }, 3000);
+            const result = await api.v1.account.unSubcribeAcounnt(params);
+            
+            setStatus(!result.success);
+            setisLoading(false);
+            if (!result.success) {
+                //@ts-ignore
+                setTitle(result?.title)
+                setMessage(result?.message)
                 }
                 
             }
 			catch (err) {
-				setStatus(false);
+				setStatus(true);
                 setisLoading(false);
             }
 		}
@@ -46,29 +44,51 @@ const Unsubscribe: React.FC = () => {
 			restoreAccount();
 		}
     }, [router.query])
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-12 min-vh-100">
-         <div className="contact-notify-success restore-account">
-            { !isLoading ? <h2 className="title">{ status ? `Message Case Already Unsubscribed` : 'Message Case Fail'}  </h2> : <Skeleton style={{width:150}} height={30} />}
-            {
-                !isLoading ?
-                    <p className="mb-50" dangerouslySetInnerHTML={
+    return (
+        <>
+            <Head>
+                <title>{
                     //@ts-ignore
-                    { __html: status ? `You have already been removed from our mailing list. If you are still getting emails in error, please contact <span class="text-color-blue">info@priceguide.cards</spann>.` : 'Invalid token - If you are having trouble removing yourself from our mailing list please contact <span class="text-color-blue">info@priceguide.cards</span> and we will manually remove you.' }}></p>   
-                    :
-                    <div>
-                        <Skeleton style={{ width: 350 }} height={30} />
-                        <Skeleton style={{ width: 350 }} height={30} />
-                    </div>         
-            }
-            
-        </div>
-        </div>
-      </div>
-    </div>
+                    props?.titlePage ?? ''}</title>
+                <meta name="description" content={
+                    //@ts-ignore
+                    props?.descriptionPage ?? ''} />
+            </Head>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 min-vh-100">
+                        <div className="contact-notify-success restore-account">
+                            {!isLoading ? <h2 className="title">{title ? `${title}` : ''}  </h2> : <Skeleton style={{ width: 150 }} height={30} />}
+                            {!isLoading ?
+                                <p className="mb-50" dangerouslySetInnerHTML={
+                                    //@ts-ignore
+                                    { __html: message ? message : '' }}></p>
+                                :
+                                <div>
+                                    <Skeleton style={{ width: 350 }} height={30} />
+                                    <Skeleton style={{ width: 350 }} height={30} />
+                                </div>}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
   );
 }
+export const getServerSideProps = async (context: any) => { 
+  try {
+   
+    let titlePage = `Unsubscribe | PriceGuide.Cards`;
+    
+    return {props:{
+     titlePage
+    }}
 
+  } catch (error) {
+    
+  }
+  return {
+    props: {},
+  };
+}
 export default Unsubscribe;
