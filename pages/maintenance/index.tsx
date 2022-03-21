8d5f56194fdd0d95cb6bed66e093c06627bc35f0
Ngoc Thai <ngocthai@pixelcent.com>
2022-03-21 17:31:56 +0700

    update status 503

--- pages/maintenance/index.tsx
+++ pages/maintenance/index.tsx
@@ -3,37 +3,50 @@ import React, { useEffect, useState } from 'react';
 import imgMaintenance from "assets/images/maintenance.svg";
 import imgInfo from "assets/images/alert-info.svg";
 import imgClose from "assets/images/cross-gray.svg";
+import { NextPage } from 'next';
+
+interface Props {
+  status?: string;
+}
 
 type PropTypes = {
 	location: any,
 }
-const MaintenancePage: React.FC<PropTypes> = (props) => {
+
+//@ts-ignore
+const MaintenancePage : NextPage<Props> = ({ status }) => {
 
   const [hideClass, setHideClass] = React.useState<string>("");
 
 	return (
-
 		<section id="maintenance">
 			<img className="imgMainIcon" src={imgMaintenance} alt="" title="" />
-            <h4>Updating Database - PriceGuide.Cards will be back shortly</h4>
-            {/* <div className="alert alert-maintenance" role="alert">
-                <img src={imgInfo} alt="" title="" />
-                <div className="content">Our database upgrade in <span className="cblue">3:00pm - 4:00pm</span> (CES)</div>
-                <span> <img className="close" src={imgClose} alt="" title="" /> </span>
-            </div> */}
+      <h4>Updating Database - PriceGuide.Cards will be back shortly</h4>
+      {/* <div className="alert alert-maintenance" role="alert">
+        <img src={imgInfo} alt="" title="" />
+        <div className="content">Our database upgrade in <span className="cblue">3:00pm - 4:00pm</span> (CES)</div>
+        <span> <img className="close" src={imgClose} alt="" title="" /> </span>
+      </div> */}
 		</section>
 	);
 }
-export async function getServerSideProps() {
 
-  try {
-    return {
-      props: {}
-    };
-  } catch(e) {
-   return {}
-  }
+//@ts-ignore
+MaintenancePage.getInitialProps = async ({res}:any) => {
 
-  //...
+  if(res) res.statusCode = 503;
+
+  return { statusCode : 503 }
+
 }
+
+// export async function getServerSideProps() {
+//   try {
+//     return {
+//       props: {}
+//     };
+//   } catch(e) {
+//    return {}
+//   }
+// }
 export default MaintenancePage;
