29a930897682f740d2579d9948b77629bd9d11f6
haito@pixelcent <minhhaizzzz98@gmail.com>
2022-03-31 14:24:24 +0700

    [Fe][818] redirect add card collection when create Portfolio

--- components/modal/chosseCollection.tsx
+++ components/modal/chosseCollection.tsx
@@ -32,7 +32,7 @@ const ChosseCollection = ({ table = "portfolio", title = "collection", isOpen, s
 
   const router = useRouter();
   
-  const getData = async () => {
+  const getData = async (isAdd:boolean = false) => {
     try {
       const params = {
         table: table,
@@ -42,6 +42,13 @@ const ChosseCollection = ({ table = "portfolio", title = "collection", isOpen, s
       if (result.success) {
         setCollections(result.data)
         setDataSearch(result.data)
+       
+        if(isAdd) {
+          let collectionNew = result.data[ result.data.length -1];
+          props.selectCollection && props.selectCollection(collectionNew)
+          setIsOpen(false);
+          setIsModal(false)
+        }
       }
       if (!result.success) {
         // @ts-ignore
@@ -66,8 +73,8 @@ const ChosseCollection = ({ table = "portfolio", title = "collection", isOpen, s
   }
 
   const onCreateSuccess = () => {
-    getData();
-    setIsOpen(true);
+    getData(true);
+    setIsOpen(false);
     setIsModal(false)
   }
 
