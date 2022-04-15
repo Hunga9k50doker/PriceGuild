f423de17d30ce85622913df1d5500d8db2c660b7
Phong Ngô <tphong0204@gmail.com>
2022-04-15 15:25:03 +0700

    [FE]#852 update filter

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1275,6 +1275,7 @@ const CardList = (props: PropTypes) => {
                         ref={playerNameRef}
                         onChange={onChangeSearch}
                         name="playerName"
+                        isButton={true}
                       />
                     </div>
                     <div className="accordion" id="CardNumberFilter">
@@ -1283,6 +1284,7 @@ const CardList = (props: PropTypes) => {
                         ref={cardNumberRef}
                         onChange={onChangeSearch}
                         name="cardNumber"
+                        isButton={true}
                       />
                     </div>
                   </div>
