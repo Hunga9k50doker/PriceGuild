cec0e665f64229b40f6ba1119de285fc3f4b4de9
Ngoc Thai <ngocthai@pixelcent.com>
2022-04-19 10:24:42 +0700

    fe task 905 - fix css datepicker

--- components/profile/addCard.tsx
+++ components/profile/addCard.tsx
@@ -1581,6 +1581,7 @@ const AddCard = ({ isEdit = false }: PropTypes) => {
                         dateFormat="MMM, d, yyyy"
                         maxDate={new Date()}
                         selected={value}
+                        disabledKeyboardNavigation
                         onChange={(e) => {
                           if (!e) {
                             onChange(new Date());
