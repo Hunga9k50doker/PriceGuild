366ff43804c3bba2d55ca72df57e346760ba4263
tompg1 <tom.a.bland@gmail.com>
2022-05-20 10:54:05 +0700

    # [984] /collections/ handled instant 404 & switch to local address API

--- pages/collections/[sport]/index.tsx
+++ pages/collections/[sport]/index.tsx
@@ -2,21 +2,34 @@ import React, { useState, useEffect } from "react";
 import Collections from "components/collections";
 import Head from 'next/head';
 
-const CollectionList = (props:any) => {
+const CollectionList = (props: any) => {
   return (
     <>
       <Head>
         <title>{props?.data?.name} Card Collections | PriceGuide.Cards</title>
         <meta name="description" content={`Browse ${props?.data?.name} card collections listed on PriceGuide.Cards`} />
       </Head>
-      <Collections/>
+      <Collections />
     </>
   );
 };
 
-export const getServerSideProps = async (context:any) => {
+export const getServerSideProps = async (context: any) => {
   try {
 
+    // Check if the sport is a valid sport via hardcode
+    const accepted_sports = ["baseball", "football", "hockey", "basketball", 
+                             "racing", "soccer", "wrestling", "multi-sport", 
+                             "non-sport", "mma", "golf", "tennis", "boxing"];
+
+    if (!accepted_sports.includes(context.query.sport)) {
+      return {
+        // returns the default 404 page with a status code of 404
+        notFound: true,
+      };
+    }
+
+    // Get the Collection List
     let parCollections = {
       is_newest: true,
       limit: 1,
@@ -32,8 +45,8 @@ export const getServerSideProps = async (context:any) => {
       },
       body: JSON.stringify(parCollections)
     }
-    
-    const res = await fetch(`${process.env.REACT_APP_API_URL}/collections/list`, config);
+
+    const res = await fetch(`${process.env.REACT_APP_API_LOCAL}/collections/list`, config);
     const data = await res.json();
     return {
       props: {
