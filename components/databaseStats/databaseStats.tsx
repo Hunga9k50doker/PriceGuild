e6e539d8f9f003349b4bcfba7a9f348dab7ab1ad
tompg1 <tom.a.bland@gmail.com>
2022-06-29 14:00:13 +0700

    # [1013] - DB Stats Updated

--- components/databaseStats/databaseStats.tsx
+++ components/databaseStats/databaseStats.tsx
@@ -2,23 +2,23 @@ import React from 'react';
 
 const DatabaseStats: React.FC = () => {
 
-  return (
-    <div className="row g-0 statistical">
-    <div className="col">
-        <div className="text-statistical"> 250M </div>
-        <div className="sub-text-statistical"> Recorded Sales Prices </div>
-    </div>
-    <div className="col">
-        <div className="text-statistical"> 10.5M </div>
-        <div className="sub-text-statistical"> Cards Featured </div>
-    </div>
-    <div className="col">
-        <div className="text-statistical"> 135K </div>
-        <div className="sub-text-statistical"> Active Collectors </div>
-    </div>
-    <div className="line-bottom"></div>
-</div>
-  );
+    return (
+        <div className="row g-0 statistical">
+            <div className="col">
+                <div className="text-statistical"> 270M </div>
+                <div className="sub-text-statistical"> Recorded Sales Prices </div>
+            </div>
+            <div className="col">
+                <div className="text-statistical"> 11.5M </div>
+                <div className="sub-text-statistical"> Cards Featured </div>
+            </div>
+            <div className="col">
+                <div className="text-statistical"> 155K </div>
+                <div className="sub-text-statistical"> Active Collectors </div>
+            </div>
+            <div className="line-bottom"></div>
+        </div>
+    );
 }
 
 export default React.memo(DatabaseStats);
